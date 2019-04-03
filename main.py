8b516e9030e62cfc434594fe866d366d0f1a6c7d
erin-a <erineadler@gmail.com>
2019-04-02 22:39:09 -0500

    it is successfully encrypting the messages input

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-def form():
-    <!DOCTYPE html>
+form = ''' 
+<!DOCTYPE html>
 
     <html>
         <head>
-            <stlye>
+            <style>
                 form {
                     background-color: #eee;
                     padding: 20px;
                     margin: 0 auto;
                     width: 540px;
-                    font 16px sans-serif;
+                    font: 16px sans-serif;
                     border-radius: 10px;
                 }
-                textarea {
-                    ,argom: 10px 0;
+                    textarea {
+                    margin: 10px 0;
                     width: 540px;
                     height: 120px;
                 }
-            </stlye>
+            </style>
         </head>
-        <body>"""
-            #create form here
-            """
+        <title>
+            Web-Caesar Assignment
+        </title>
+
+        <body>
+            <!-- create your form here -->
+            <form action="/" method="POST">
+                <label> Rotate by: 
+                <input type="text" name="rot" value="0" />
+                </label>
+                <textarea name="text"></textarea>
+            <input type="submit" value="Submit Query" />
+            </form>
         </body>
-        </html>
-
-
+    </html>
+    '''
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
 
+@app.route("/", methods=["POST"])
+def encrypt():
+    rot = int(request.form['rot'])
+    text = str(request.form['text'])
+    cypher = rotate_string(text, rot)
 
+    return "<h1>" + cypher + "</h1>"
 
 app.run()
